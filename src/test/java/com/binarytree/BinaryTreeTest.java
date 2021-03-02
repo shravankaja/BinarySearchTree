a781e3b54d7a5c43faa343963bb5f1f2c3477f85
shravan-kaja <shravan.kaja@gmail.com>
2021-03-02 22:07:53 +0530

    Added: nodes to tree according to levels or depth and ensure sie

--- src/test/java/com/binarytree/BinaryTreeTest.java
+++ src/test/java/com/binarytree/BinaryTreeTest.java
@@ -6,11 +6,21 @@ public class BinaryTreeTest {
 
     @Test
     void test() {
-        BinarySearchTree binarySearchTree=new BinarySearchTree();
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
         binarySearchTree.addNodeRecursiveFashion(56);
         binarySearchTree.addNodeRecursiveFashion(30);
-        binarySearchTree.addNodeRecursiveFashion(76);
-        int size=binarySearchTree.size();
-        Assertions.assertEquals(3,size);
+        binarySearchTree.addNodeRecursiveFashion(70);
+        binarySearchTree.addNodeRecursiveFashion(22);
+        binarySearchTree.addNodeRecursiveFashion(40);
+        binarySearchTree.addNodeRecursiveFashion(60);
+        binarySearchTree.addNodeRecursiveFashion(95);
+        binarySearchTree.addNodeRecursiveFashion(11);
+        binarySearchTree.addNodeRecursiveFashion(65);
+        binarySearchTree.addNodeRecursiveFashion(3);
+        binarySearchTree.addNodeRecursiveFashion(16);
+        binarySearchTree.addNodeRecursiveFashion(63);
+        binarySearchTree.addNodeRecursiveFashion(67);
+        int size = binarySearchTree.size();
+        Assertions.assertEquals(13, size);
     }
 }
